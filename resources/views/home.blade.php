498263750d93a21a9043c17b8d78fffbd4dbd0f2
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-23 04:10:34 +0430

    remove extra code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,5 @@
     <x-home.hero></x-home.hero>
     <x-home.about></x-home.about>
     <x-home.portfolio></x-home.portfolio>
-    {{-- <x-home.tutorial-video></x-home.tutorial-video> --}}
     <x-home.contact></x-home.contact>
 </x-app-layout>
